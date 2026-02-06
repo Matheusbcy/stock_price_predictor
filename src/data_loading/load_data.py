e717237039085f6dcd98b28d81032f47afe5555e
Matheus <matheusbcy@gmail.com>
2026-02-06 06:49:14 -0300

    DVC part 2

--- src/data_loading/load_data.py
+++ src/data_loading/load_data.py
@@ -2,6 +2,7 @@ import logging
 
 import numpy as np
 import pandas as pd
+import time
 
 logger = logging.getLogger("src.data_loading.load_data")
 
@@ -10,19 +11,20 @@ def fetch_data() -> pd.DataFrame:
     logger.info("Fetching data...")
     dataset = pd.read_csv("data/petr4_treinamento.csv")
     dataset = dataset.dropna()
-    return dataset
+
+    data = dataset.sample(frac=1, random_state=int(time.time())).reset_index(drop=True)
+    
+    return data
 
 def save_data(data: pd.DataFrame) -> None:
     output_path = "data/raw/raw.csv"
     logger.info(f"Saving raw data to {output_path}")
     data.to_csv(output_path, index=False)
 
-
 def main() -> None:
     raw_data = fetch_data()
     save_data(raw_data)
     logger.info("Data loading completed")
-
 
 if __name__ == "__main__":
     main()
